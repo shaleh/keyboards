7b86d7c1b01b611d7c6d8773273faf08133ccebf
Sean Perry <shalehperry@gmail.com>
2024-05-17 22:27:19 -0700

    Callum style voyager

--- keyboards/zsa/voyager/keymaps/colemak-dh/keymap.c
+++ keyboards/zsa/voyager/keymaps/colemak-dh/keymap.c
@@ -10,38 +10,39 @@ static const char my_with_name[] PROGMEM = personal_user_name_full;
 
 enum custom_keycodes {
     RGB_SLD = SAFE_RANGE,
-    HSV_0_255_255,
-    HSV_74_255_255,
-    HSV_169_255_255,
+    HSV_1,
+    HSV_2,
+    HSV_3,
     WORK_EMAIL,
     PERSONAL_EMAIL,
     DBL_CLN,
     DIR_UP,
     ARROW,
+    NOT_EQL,
 };
 
-
-#define DBLQT S(KC_QUOTE)
 #define TLC TOGGLE_LAYER_COLOR
 
 #define B_DOWN KC_BRIGHTNESS_DOWN
 #define B_UP   KC_BRIGHTNESS_UP
 
-#define V_DOWN KC_AUDIO_VOL_DOWN
-#define V_UP   KC_AUDIO_VOL_UP
-#define V_MUTE KC_AUIDIO_MUTE
+#define HOME G(KC_LEFT)
+#define END G(KC_RGHT)
+#define FWD G(KC_RBRC)
+#define BACK G(KC_LBRC)
+#define TAB_L G(S(KC_LBRC))
+#define TAB_R G(S(KC_RBRC))
+#define SPC_L G(KC_LEFT)
+#define SPC_R G(KC_RGHT)
+#define LA_SYM MO(SYM_F)
+#define LA_NAV MO(SYS_NAV)
 
-#define L_ENTER LT(SYM_F,    KC_ENTER)
-#define L_SPACE LT(SYS_MOVE, KC_SPACE)
-#define L_MINUS LT(SPECIAL,  KC_MINUS)
+#define OS_SHIFT OSM(MOD_LSFT)
+#define OS_CTRL OSM(MOD_LCTL)
+#define OS_ALT  OSM(MOD_LALT)
+#define OS_CMD  OSM(MOD_LGUI)
 
-#define M_QT    MT(MOD_LSFT, KC_QUOTE)
-#define M_Z     MT(MOD_LGUI, KC_Z)
-#define M_X     MT(MOD_LALT, KC_X)
-#define M_SLASH MT(MOD_RGUI, KC_SLASH)
-#define M_DOT   MT(MOD_RALT, KC_DOT)
-
-enum layers {BASE = 0, SYM_F, SYS_MOVE, SPECIAL};
+enum layers {BASE = 0, SYM_F, SYS_NAV, SPECIAL};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*                                                                                                                                */
@@ -51,25 +52,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* └─────────────────────────────────────────────────┘       │╰╯╰╯╰╯│                                                             */
 /* ┌──────────┬─────────┬─────────┬─────────┬─────────┬──────╨──┐┌──╨──────┬─────────┬─────────┬─────────┬─────────┬────────────┐ */
 /* │   ESC    │   1/!   │   2/@   │   3/#   │   4/$   │   5/%   ││   6/^   │   7/&   │   8/\*  │   9/(   │   0/)   │    -/_     │ */
-     QK_GESC,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     L_MINUS,
-/* │   `/~    │         │         │         │         │         ││         │         │         │         │         │ L(SPECIAL) │ */
+     QK_GESC,    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,      KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,
+/* │   `/~    │         │         │         │         │         ││         │         │         │         │         │            │ */
 /* ├──────────┼─────────┼─────────┼─────────┼─────────┼─────────┤├─────────┼─────────┼─────────┼─────────┼─────────┼────────────┤ */
-/* │ CAPSWORD │    Q    │    W    │    F         P         B    ││    J         L         U         Y        ;           \        */
-     CW_TOGG,    KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,      KC_J,     KC_L,     KC_U,     KC_Y,    KC_SCLN,   KC_BSLS,
+/* │   TAB    │    Q    │    W    │    F         P         B    ││    J         L         U         Y         ;          \        */
+      KC_TAB,    KC_Q,     KC_W,     KC_F,     KC_P,     KC_B,      KC_J,     KC_L,     KC_U,     KC_Y,    KC_SCLN,   KC_BSLS,
 /* │          │         │         │         │         │         ││         │         │         │         │         │              */
 /* ├──────────┼─────────┼─────────┼─────────┼─────────┼─────────┤├─────────┼─────────┼─────────┼─────────┼─────────┼────────────┤ */
-/* │  SHIFT   │    A    │    R    │    S    │    T    │    G    ││    M    │    N    │    E    │    I    │    O    │    '/"     │ */
-     KC_LSFT,    KC_A,     KC_R,     KC_S,    KC_T,      KC_G,      KC_M,     KC_N,     KC_E,     KC_I,     KC_O,       M_QT,
-/* │          │         │         │         │         │         ││         │         │         │         │         │   Shift    │ */
+/* │ CAPSWORD │    A    │    R    │    S    │    T    │    G    ││    M    │    N    │    E    │    I    │    O    │    '/"     │ */
+     CW_TOGG,    KC_A,     KC_R,     KC_S,    KC_T,      KC_G,      KC_M,     KC_N,     KC_E,     KC_I,     KC_O,     KC_QUOT,
+/* │          │         │         │         │         │         ││         │         │         │         │         │            │ */
 /* ├──────────┼─────────┼─────────┼─────────┼─────────┼─────────┤├─────────┼─────────┼─────────┼─────────┼─────────┼────────────┤ */
-/* │ Control  │   Z     │    X    │    C    │    D    │    V    ││    K    │    H    │   ,/<   │   ./>   │   //?   │  Control   │ */
-     KC_LCTL,     M_Z,      M_X,     KC_C,     KC_D,     KC_V,       KC_K,    KC_H,   KC_COMMA,   M_DOT,   M_SLASH,   KC_RCTL,
-/* │          │   GUI   │ CMD/Alt │         │         │         ││         │         │         │ CMD/ALT │   GUI   │            │ */
+/* │   ../    │    Z    │    X    │    C    │    D    │    V    ││    K    │    H    │   ,/<   │   ./>   │   //?   │            │ */
+      DIR_UP,    KC_Z,     KC_X,     KC_C,     KC_D,     KC_V,      KC_K,     KC_H,   KC_COMMA,   KC_DOT,  KC_SLSH,    KC_ENT,
+/* │          │         │         │         │         │         ││         │         │         │         │         │            │ */
 /* └──────────┴─────────┴─────────┴─────────┴─────────┴─────────┘└─────────┴─────────┴─────────┴─────────┴─────────┴────────────┘ */
 /*                                  ┌─────────────┬─────────────┐┌─────────────┬─────────────┐                                    */
-/*                                  │    TAB      │    Enter    ││    SPACE    │  Backspace  │                                    */
-                                        KC_TAB,       L_ENTER,       L_SPACE,      KC_BSPC
-/*                                  │             │   L(SYM_F)  ││ L(SYS_MOVE) │             │                                    */
+/*                                  │             │    Shift    ││    SPACE    │             │                                    */
+                                        LA_NAV,       KC_LSFT,       KC_SPC,       LA_SYM
+/*                                  │             │             ││             │             │                                    */
 /*                                  └─────────────┴─────────────┘└─────────────┴─────────────┘                                    */
    ),
 
@@ -78,17 +79,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* │ SYM_F                                           │       ╭╮╭╮╭╮╭╮                                                             */
 /* └─────────────────────────────────────────────────┘       │╰╯╰╯╰╯│                                                             */
 /* ┌──────────┬─────────┬─────────┬─────────┬─────────┬──────╨──┐┌──╨──────┬─────────┬─────────┬─────────┬─────────┬────────────┐ */
-/* │   ESC    │    F1   │    F2   │   F3    │    F4   │   F5    ││   F6    │   F7    │   F8    │   F9    │   F10   │    F11     │ */
-     _______,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,     KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,     KC_F11,
+/* │    F1    │    F2   │    F3   │   F4    │    F5   │   F6    ││   F7    │   F8    │   F9    │   F10   │   F11   │    F12     │ */
+      KC_F1,     KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,     KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,    KC_F12,
 /* ├──────────┼─────────┼─────────┼─────────┼─────────┼─────────┤├─────────┼─────────┼─────────┼─────────┼─────────┼────────────┤ */
-/* │          │    `    │    <    │   >     │    "    │    .    ││    &    │   ::    │   [     │    ]    │    %    │    F12     │ */
-     _______,   KC_GRV,   KC_LABK,  KC_RABK,   DBLQT,   KC_DOT,    KC_AMPR,  DBL_CLN, KC_LBRC,   KC_RBRC,  KC_PERC,    KC_F12,
+/* │          │    @    │    [    │    {    │    (    │    ~    ││    ^    │    )    │    }    │    ]    │         │            │ */
+     _______,    KC_AT,   KC_LBRC,  KC_LCBR,  KC_LPRN,  KC_TILD,   KC_CIRC,  KC_RPRN,  KC_RCBR,  KC_RBRC,  _______,    _______,
 /* ├──────────┼─────────┼─────────┼─────────┼─────────┼─────────┤├─────────┼─────────┼─────────┼─────────┼─────────┼────────────┤ */
-/* │          │    !    │    -    │   +     │    =    │    #    ││    |    │    :    │   (     │    )    │    ?    │   ARROW    │ */
-     _______,   KC_EXLM,  KC_MINS,  KC_PLUS,  KC_EQL,   KC_HASH,   KC_PIPE,  KC_COLN,  KC_LPRN,  KC_RPRN,  KC_QUES,    ARROW,
+/* │          │    -    │    *    │    =    │    _    │    $    ││    #    │   CMD   │   ALT   │   CTRL  │  SHIFT  │   ARROW    │ */
+     _______,   KC_MINS,  KC_ASTR,  KC_EQL,   KC_UNDS,  KC_DLR,    KC_HASH,   OS_CMD,   OS_ALT,  OS_CTRL,  OS_SHIFT,   ARROW,
 /* ├──────────┼─────────┼─────────┼─────────┼─────────┼─────────┤├─────────┼─────────┼─────────┼─────────┼─────────┼────────────┤ */
-/* │          │    ^    │    /    │   *     │    \    │   ../   ││    ~    │    $    │    {    │    }    │    @    │            │ */
-     _______,   KC_CIRC, KC_SLASH,  KC_ASTR,  KC_BSLS,  DIR_UP,    KC_TILD,  KC_DLR,   KC_LCBR,  KC_RCBR,   KC_AT,    _______,
+/* │          │    +    │    /    │   !=    │    |    │    %    ││         │    \    │    &    │    :    │    !    │    ::      │ */
+     _______,   KC_PLUS,  KC_SLASH, NOT_EQL,  KC_PIPE,  KC_PERC,   _______,  KC_BSLS,  KC_AMPR,  KC_COLN,  KC_EXLM,   DBL_CLN,
 /* └──────────┴─────────┴─────────┘─────────┴─────────┴─────────┘└─────────┴─────────┴─────────┘─────────┴─────────┴────────────┘ */
 /*                                          ┌─────────┬─────────┐┌─────────┬─────────┐                                            */
 /*                                          │         │         ││         │         │                                            */
@@ -96,21 +97,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /*                                          └─────────┴─────────┘└─────────┴─────────┘                                            */
    ),
 
-   [SYS_MOVE] = LAYOUT(
+   [SYS_NAV] = LAYOUT(
 /* ┌─────────────────────────────────────────────────┐                                                                            */ 
-/* │ SYS_MOVE                                        │       ╭╮╭╮╭╮╭╮                                                             */
+/* │ SYS_NAV                                         │       ╭╮╭╮╭╮╭╮                                                             */
 /* └─────────────────────────────────────────────────┘       │╰╯╰╯╰╯│                                                             */
 /* ┌──────────┬─────────┬─────────┬─────────┬─────────┬──────╨──┐┌──╨──────┬─────────┬─────────┬─────────┬─────────┬────────────┐ */
-     RGB_TOG,   RGB_MOD,    TLC,    _______,  B_DOWN,    B_UP,     KC_VOLD,  KC_VOLU,  KC_MUTE,  _______,  _______,   _______,
+     RGB_TOG,   RGB_MOD,    TLC,    _______,  _______,  KC_MUTE,   B_DOWN,    B_UP,    _______,  _______,  _______,   _______,
 /* ├──────────┼─────────┼─────────┼─────────┼─────────┼─────────┤├─────────┼─────────┼─────────┼─────────┼─────────┼────────────┤ */
-/* │          │         │         │         │         │         ││         │         │         │         │         │            │ */
-     _______,   _______,   _______, _______,  _______,  _______,   KC_PGUP,  KC_HOME,  _______,  KC_END,   _______,   _______,
+/* │          │         │         │  TAB L  │  TAB R  │ VOL UP  ││         │         │         │         │         │    DEL     │ */
+      HSV_1,    _______,  _______,   TAB_L,    TAB_R,   KC_VOLU,   QK_BOOT,  KC_HOME,  KC_PGUP,  KC_END,   KC_PGDN,    KC_DEL,
 /* ├──────────┼─────────┼─────────┼─────────┼─────────┼─────────┤├─────────┼─────────┼─────────┼─────────┼─────────┼────────────┤ */
-/* │          │         │         │         │         │         ││         │         │         │         │         │            │ */
-     _______,   _______,  _______,  _______,  _______,  _______,   KC_PGDN,  KC_LEFT,   KC_UP,   KC_DOWN,  KC_RIGHT,  _______,
+/* │          │  SHIFT  │  CTRL   │  ALT    │   CMD   │ VOL DN  ││         │         │         │         │         │ BACKSPACE  │ */
+      HSV_2,   OS_SHIFT,  OS_CTRL,  OS_ALT,   OS_CMD,   KC_VOLD,   _______,  KC_LEFT,   KC_UP,   KC_DOWN,  KC_RIGHT,  KC_BSPC,
 /* ├──────────┼─────────┼─────────┼─────────┼─────────┼─────────┤├─────────┼─────────┼─────────┼─────────┼─────────┼────────────┤ */
-/* │          │         │         │         │         │         ││         │         │         │         │         │            │ */
-     _______,   _______,  _______,  HSV_0_255_255, HSV_74_255_255, HSV_169_255_255, LCTL(LSFT(KC_TAB)), LCTL(KC_TAB), KC_MS_WH_UP, KC_MS_WH_DOWN, _______,  _______,
+/* │          │ SPACE L │ SPACE R │         │         │  PLAY   ││         │         │         │         │         │            │ */
+      HSV_3,     SPC_L,    SPC_R,    BACK,      FWD,    KC_MPLY,   _______,  _______,  _______,  _______,  _______,  _______,
 /* └──────────┴─────────┴─────────┘─────────┴─────────┴─────────┘└─────────┴─────────┴─────────┘─────────┴─────────┴────────────┘ */
 /*                                          ┌─────────┬─────────┐┌─────────┬─────────┐                                            */
 /*                                          │         │         ││         │         │                                            */
@@ -138,6 +139,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    )
 };
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, SYM_F, SYS_NAV, SPECIAL);
+}
+
 bool my_handle_key_pressed_user(uint16_t keycode, keyrecord_t *record) {
     const uint8_t mods = get_mods();
     const uint8_t all_mods = (mods | get_weak_mods()
@@ -153,17 +158,17 @@ bool my_handle_key_pressed_user(uint16_t keycode, keyrecord_t *record) {
           rgblight_mode(1);
           return false;
         }
-        case HSV_0_255_255: {
+        case HSV_1: {
           rgblight_mode(1);
           rgblight_sethsv(0,255,255);
           return false;
         }
-        case HSV_74_255_255: {
+        case HSV_2: {
           rgblight_mode(1);
           rgblight_sethsv(74,255,255);
           return false;
         }
-        case HSV_169_255_255: {
+        case HSV_3: {
           rgblight_mode(1);
           rgblight_sethsv(169,255,255);
           return false;
@@ -187,6 +192,10 @@ bool my_handle_key_pressed_user(uint16_t keycode, keyrecord_t *record) {
         }
         case DIR_UP: {
           SEND_STRING("../");
+          return false;
+        }
+        case NOT_EQL: {
+          SEND_STRING("!=");
           return false;
         }
         case ARROW: {
